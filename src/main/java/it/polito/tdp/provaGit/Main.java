022f4e3fa51ead55835e73c7c2b57ef471733a05
HP <HP@LAPTOP-VNFIM2PU.homenet.telecomitalia.it>
2022-10-12 16:10:46 +0200

    Aggiunti commenti

--- src/main/java/it/polito/tdp/provaGit/Main.java
+++ src/main/java/it/polito/tdp/provaGit/Main.java
@@ -8,21 +8,29 @@ public class Main {
 
 //Voglio creare un REPOSITORY LOCALE da agganciare al progetto
 //Inizializzazione del REPOSITORY
-//Tasto destro sul progetto -> Team -> Share Project -> Creare il REPOSITORY
-//sulla cartella dove è presente il progetto (in modo da tenerli legati).
+//Tasto destro sul progetto -> Team -> Share Project -> A questo punto Eclipse ci chiede dove vogliamo creare/salvare il repository.
+//E' buona prassi creare il REPOSITORY sulla cartella dove è presente il progetto (in modo da tenerli legati) spuntando la casellina.
 
-//I file acquisiscono un punto interrogativo. I file non sono utilizzati, quindi Eclipse
+//Vicino al progetto appare un bidoncino giallo, che corrisponde al repository
+//I file acquisiscono un punto interrogativo. I file non sono utilizzati, il repository è vuoto e quindi Eclipse
 //non sa se vadano posizionati nel repository... Questo è lo stato iniziale della working copy 
 //prima del primo COMMIT (inserimento della working copy nel repository)
 
 //Fare quindi il primo COMMIT (tasto destro -> Team -> Commit).
 
-//Appare una finestrella di Commit: in basso a destra si trova l'autore che ha effettuato il commit.
+//Appare una finestrella di Commit: in basso a destra si trova l'autore che ha effettuato il Commit.
 //A sinistra in alto (Unstaged Changes) ci sono tutti i file cambiati dall'ultimo commit (per il primo tutti).
-//Nella parte bassa(Staged Changes) sono presenti tutti i file cambiati che io voglio immettere nel Repository (in questo caso tutti).
-//Basta trascinarli dentro, di questi file sarà fatto il commit, cosa che avverrà anche nelle future modifiche. (il doppio + li aggiunge tutti).
+//Nella parte bassa (Staged Changes) sono presenti tutti i file cambiati che io voglio immettere nel Repository (in questo caso tutti).
+//Basta trascinarli dentro (il doppio + li aggiunge tutti), di questi file sarà fatto il commit. A questo punto Eclipse si ricorderà di questi file e nei prossimi Commit saranno già posizionati in questa casella per le future modifiche.
 
-//In alto a destra è presente la finestra "Commit Message", ovvero il messaggio/commento che accompagna il mio Commit (spiego le modifiche).
+//In alto a destra è presente la finestra "Commit Message", ovvero il messaggio/commento che accompagna il mio Commit (spiego le modifiche). Per il primo Commit posso scrivere "Versione iniziale".
 
-//Dopo che effettuo il Commit, i punti interrogativi diventano dei + (file di progetto aggiunti al repository).
-//Quando modifico uno dei file precedentemente "Committati", appare a sinistra il simbolo >, che mi indica appunto che il file è stato modificato dall'ultimo Commit.'
+//Dopo che ho fatto ste cose effettuo il commit in basso a destra. Dopo che effettuo il Commit, i punti interrogativi diventano dei + (file di progetto aggiunti al repository).
+//Quando modifico uno dei file precedentemente "Committati", appare a sinistra il simbolo >, che mi indica appunto che il file è stato modificato dall'ultimo Commit.
+
+//Se voglio salvare le modifiche sul Repository bisogna sempre fare Team-> Commit (RICORDARSI SEMPRE IL MESSAGGIO).
+
+//Al momento ho lavorato in locale, ora voglio salvare su GitHub.
+
+//Per salvare su GitHub vado su Team -> Push (ho già salvato nome utente e password).
+//Dopo il primo Push (che di solito avrà il nome "origin"), io quando faccio nuovi Commit posso scegliere di fare "Commit and Push", facendo insieme le operazioni.
